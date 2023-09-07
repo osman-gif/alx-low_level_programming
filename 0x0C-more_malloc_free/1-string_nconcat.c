2f2899e3b5eaafb77bd7f3d7229298fcea7a7060
osman-gif <osmanngong@gmail.com>
2023-09-07 19:00:10 +0200

    modified the 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,46 +23,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	/* get the size of s1 */
 	for (size = 0; s1[size] != '\0'; size++)
 	{
 	}
 
-	/**
-	 * Assign the address of the allocated memory
-	 * to the pointer ptr
-	 */
-
 	ptr = malloc(size + n + 1);
-
-	/*Check if malloc fails*/
 
 	if (ptr == NULL)
 		return (NULL);
 
-	/**
-	 * Assign the s1 to the first size bytes of ptr
-	 */
-
 	for (j = 0; s1[j] != '\0'; j++)
 		ptr[j] = s1[j];
-
-	/**
-	 * Concatenate the n bytes of the s2 to s1
-	 * starting from size + 1 byte of s1
-	 */ 
 
 	for (i = 0; i < n && s2[i] != '\0'; i++)
 	{
 		ptr[size++] = s2[i];
 	}
-
 	if (i < n)
 		ptr[size] = '\0';
-
 	if (i <= n && s2[i] != '\0')
 		ptr[size] = '\0';
-
-
 return (ptr);
 }
