227abc596ed8254293e2d2b01b911d72c8bf9f2d
osman-gif <osmanngong@gmail.com>
2023-09-11 15:20:43 +0200

    Modified the dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,9 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog;
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *onwer);
 void free_dog(dog_t *d);
