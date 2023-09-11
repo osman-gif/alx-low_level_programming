db435234014bc48caa1a1a657f029007a441879c
osman-gif <osmanngong@gmail.com>
2023-09-11 15:05:11 +0200

    Modified the 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,6 +9,13 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
-	d->age = age;
 	d->owner = owner;
+
+	if (name == NULL)
+		d->name = "";
+
+	if (owner == NULL)
+		d->owner = "";
+
+	d->age = age;
 }
