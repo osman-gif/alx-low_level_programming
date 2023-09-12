a377156a3e08b83ecfa67246936d36b2b2d4b593
osman-gif <osmanngong@gmail.com>
2023-09-12 16:44:28 +0200

    Modified the 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+void init_alloc(int height, int width, int **ptr);
 /**
  * alloc_grid - allocate memory to 2d array
  * @width: number of columns
@@ -11,33 +11,46 @@
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = (int **)malloc(sizeof(int *) * height);
+
+	ptr = (int **)malloc(height * sizeof(int *));
 
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
+
+	init_alloc(height, width, ptr);
+
+	return (ptr);
+}
+
+/**
+ * init_alloc - Initializes a 2 dimentional array of integers
+ * @height: Number of rows of the 2d array
+ * @width: Number of columns of the 2d array
+ * @ptr: Pointer to the 2d array
+ */
+
+void init_alloc(int height, int width, int **ptr)
+{
+	int i, j;
+
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(sizeof(int) * width);
+		ptr[i] = malloc(width * sizeof(int));
 
 		if (ptr[i] == NULL)
 		{
-			free(ptr[i]);
-			return (NULL);
+			return;
 		}
-	}
 
-	if (ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
 
-	return (ptr);
+	}
 }
