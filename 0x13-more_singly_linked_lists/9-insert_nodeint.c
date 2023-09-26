e5162fd8b1e8e0b3d1f905badcfd0fa0c822d95a
osman-gif <osmanngong@gmail.com>
2023-09-26 21:21:36 +0200

    Modified the 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,13 +10,13 @@ unsigned int listlen(listint_t *head);
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i, size;
+	unsigned int __attribute__((unused)) i, size;
 	listint_t *current, *new, *tmp;
 
 	size = listlen(*head);
 
-	if (idx > size)
-		return (NULL);
+	/**if (idx > size)
+		return (NULL);**/
 
 	current = *head;
 
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx && current != NULL; i++)
 	{
 		current = current->next;
 		tmp = current->next;
