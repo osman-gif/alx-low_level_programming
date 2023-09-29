db1d2d07addb163f7067cca07d6462b9bccde17b
osman-gif <osmanngong@gmail.com>
2023-09-30 00:43:34 +0200

    Modified the 3-set_bit.c file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,11 +9,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num, num1;
-
-	if (index == 0)
-		num = 1;
-	else
-		num = 1 << index;
+	if (index > 63)
+		return (-1);
+	num = 1 << index;
 	num1 = *n;
 
 	*n = num | *n;
