d7077c7dbe66f6c65577a80d1e4dc21a78db3f82
osman-gif <osmanngong@gmail.com>
2023-09-30 01:35:26 +0200

    Modified the main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 int _putchar(int c);
 int clear_bit(unsigned long int *n, unsigned int);
+int get_endianness(void);
 int set_bit(unsigned long int *n, unsigned int);
 int get_bit(unsigned long int, unsigned int);
 void print_binary(unsigned long int n);
