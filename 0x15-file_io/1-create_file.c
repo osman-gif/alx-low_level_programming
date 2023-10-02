8e847998f05b7f79c24a675b092ac45f3d92feec
osman-gif <osmanngong@gmail.com>
2023-10-02 20:18:26 +0200

    Modified the 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 	int f_desc;
 	size_t count;
 
+	if (filename == NULL)
+		return (-1);
 	count = char_count(text_content);
 
 	f_desc = open(filename,O_CREAT|O_RDWR, S_IRUSR| S_IWUSR);
