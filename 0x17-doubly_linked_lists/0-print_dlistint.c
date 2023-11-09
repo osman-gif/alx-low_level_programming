fcdc58bb5c49ea027449b0957e44680d64d50317
osman-gif <osmanngong@gmail.com>
2023-11-09 16:23:38 +0200

    Modified the 0-print_dlistint.c file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: First node in the dlistint_t list
+ * Return: return number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
@@ -6,7 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 	if (!h)
 		return (0);
 	count = 0;
-	while(h)
+	while (h)
 	{
 		count++;
 		printf("%d\n", h->n);
