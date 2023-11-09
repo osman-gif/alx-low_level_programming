f9c773f948e868abc816270dfbb911418fe5640c
osman-gif <osmanngong@gmail.com>
2023-11-09 19:48:27 +0200

    Modified the 2-add_dnodeint.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
-include "lists.h"
+#include "lists.h"
 /**
  * add_dnodeint - adds a new node at the beginning of a listint_t list
  * @head: first node in the list
  * @n: number of the elements
  * Return:  address of the new element, or NULL if it failed
  */
-listint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
